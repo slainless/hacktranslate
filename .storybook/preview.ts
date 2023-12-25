d003fd31bd5d4597120ffd646f1e41f21baef6c8
Ahmad Fauzy <aiman.fauzy.55@gmail.com>
2023-12-25 17:22:41 +0800

    storybook update
    - added packages `sass` and `vite-tsconfig-paths`
    - removed default stories
    - refactored storybook into typescript
    - configured storybook's vite to use tsconfig paths

---
+++ .storybook/preview.ts
@@ -0,0 +1,17 @@
+import { Preview } from '@storybook/web-components'
+import '../assets/index'
+import '../assets/index.scss'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
